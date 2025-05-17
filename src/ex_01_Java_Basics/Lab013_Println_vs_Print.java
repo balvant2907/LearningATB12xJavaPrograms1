d0b7a2c532d59a3fcd41b54d8ef9821c8c008ac5
Balwant Wangikar <wangikarbalvant@gmail.com>
2025-05-17 07:40:51 +0530

    git 2nd commit

---
+++ src/ex_01_Java_Basics/Lab013_Println_vs_Print.java
@@ -0,0 +1,12 @@
+package ex_01_Java_Basics;
+
+public class Lab013_Println_vs_Print {
+
+    public static void main(String[] args) {
+        System.out.println("HI");
+        System.out.println("Bye");  //Nextline
+
+        System.out.print("Hi");
+        System.out.print("Bye"); //Sameline
+    }
+}
